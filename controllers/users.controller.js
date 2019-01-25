1fdf52de0c48645c9f39d4930b952219172dbd84
Monkey <duyk16@gmail.com>
2019-01-25 14:43:11 +0700

    get list users and patch user

--- controllers/users.controller.js
+++ controllers/users.controller.js
@@ -37,8 +37,17 @@ exports.createUser = async (req, res) => {
     id: data._id
   })
 }
-exports.getUsers = (req, res) => {
+exports.getListUsers = (req, res) => {
+  let limit = req.query.limit && req.query.limit < 100 ? parseInt(req.query.limit) : 10
+  let page = 0
+  if (req.query.page) {
+    req.query.page = parseInt(req.query.page);
+    page = Number.isInteger(req.query.page) ? req.query.page : 0;
+  }
 
+  UserModel.find().limit(limit).skip(limit * page)
+    .then(result => res.status(200).send(result))
+    .catch(err => console.log(err))
 }
 exports.getById = (req, res) => {
   UserModel.findById({_id: req.params.id})
@@ -47,4 +56,29 @@ exports.getById = (req, res) => {
       data.password = undefined
       res.status(200).send(data)
     }).catch(() => res.status(500).send('Try again after few moment'))
+}
+exports.patchById = async (req, res) => {
+  if (req.body.password) {
+    let salt = crypto.randomBytes(16).toString('base64')
+    let hash = crypto.createHmac('sha512', salt)
+                    .update(req.body.password)
+                    .digest('base64')
+    req.body.password = salt + '$' + hash
+  }
+  
+  let user = await UserModel.findById(req.params.id)
+  if (!user) return res.status(400).send({error: 'ID not found'})
+
+  // Modify data
+  for (let i in req.body) {
+    user[i] = req.body[i]
+  }
+
+  try {
+    user.save()
+    return res.status(204).send({status: 'success'})
+  } catch (error) {
+    return res.status(500).send(error)
+  }
+  
 }
--- routes/users.route.js
+++ routes/users.route.js
@@ -3,7 +3,8 @@ const route = express.Router()
 
 const userController = require('../controllers/users.controller')
 
+route.get('/', userController.getListUsers)
 route.post('/', userController.createUser)
 route.get('/:id', userController.getById)
-
+route.patch('/:id', userController.patchById)
 module.exports = route
